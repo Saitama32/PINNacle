bf6ea53e3c4c10f4aff937473d961e8f9909330e
Saitama32 <rostik743@gmail.com>
2026-03-19 23:43:16 +0300

    change ns2d LT for search tol

--- experiments/NavierStokes/ns2d_longtime_chain.py
+++ experiments/NavierStokes/ns2d_longtime_chain.py
@@ -1,21 +1,8 @@
 import os
 import sys
-import time
-import argparse
-import dill
-import numpy as np
-import torch
-import deepxde as dde
-
 os.environ["DDEBACKEND"] = "pytorch"
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
 from comet_ml import start
-from src.pde.ns import NS2D_LongTime
-from src.utils.args import parse_hidden_layers
-from src.utils.callbacks import TesterCallback, PlotCallback, LossCallback
-from rl_trainer import train_process_rl
-
 
 experiment = start(
     api_key="aP71fQTYPNqfsYWvudPPmoBl5",
@@ -26,10 +13,29 @@ experiment = start(
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(project_root)
 
+
+import time
+import argparse
+import dill
+import numpy as np
+import torch
+import deepxde as dde
+
+
+
+from src.pde.ns import NS2D_LongTime
+from src.utils.args import parse_hidden_layers
+from src.utils.callbacks import TesterCallback, PlotCallback, LossCallback
+from rl_trainer import train_process_rl
+
+
+
+
+
 experiment.log_parameters({
     "param": "v_1",
     "reward_function": "v_2",
-    "description": "farm_transitions_ns2d_longtime_rl_optimizer",
+    "description": "tolerance_ns2d_longtime_rl_optimizer",
 })
 
 
