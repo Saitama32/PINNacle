cb2f3427ba6cbdcde8f7ed77f0de75c1023d972f
Saitama32 <rostik743@gmail.com>
2026-03-19 22:35:09 +0300

    Change proj name

--- experiments/HeatConductivity/heat2d_multiscale_chain.py
+++ experiments/HeatConductivity/heat2d_multiscale_chain.py
@@ -6,7 +6,7 @@ from comet_ml import start
 
 experiment = start(
     api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-    project_name="rlpinn_heat2d_multiscale_tolerance",
+    project_name="rlpinn_heat2d_multiscale_tolerance_corrected",
     workspace="saitama32",
 )
 
