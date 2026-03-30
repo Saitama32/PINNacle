8c8aaab18446b465b60a105f283ef9923630e3a1
Saitama32 <rostik743@gmail.com>
2026-03-30 22:22:43 +0300

    Change keys and seeds

--- experiments/Burgers/burgers2d_parallel.sh
+++ experiments/Burgers/burgers2d_parallel.sh
@@ -4,8 +4,8 @@
 SCRIPT="experiments/Burgers/burgers2d_chain.py"
 
 LOG_KEY="true"
-EXP_KEY_1="67c5de4ff56f43d99aa9a296ed660236"
-EXP_KEY_2="18510e6655cc492f89c6edcc4ed92a98"
+EXP_KEY_1="8fd177530bcc430698600d74d02a874b"
+EXP_KEY_2="78ef00c1b6b54f5289864517324b933a"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
