db7d3ddf2fc686501d9b384480fc23d2421c12d0
Saitama32 <rostik743@gmail.com>
2026-03-31 01:45:14 +0300

    cnange agent keys and seeds

--- experiments/Wave/wave2d_heterogeneous_parallel.sh
+++ experiments/Wave/wave2d_heterogeneous_parallel.sh
@@ -5,7 +5,7 @@ SCRIPT="experiments/Wave/wave2d_heterogeneous_chain.py"
 
 LOG_KEY="true"
 EXP_KEY_1="5f8a6412cdff4209a31e660da18b6800"
-EXP_KEY_2="d506b5beeb5e4cfa83fcd392054abc5b"
+EXP_KEY_2="8bc0294c26144313a57f6316dbd8e7d5"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
