a81179a70e6a9358a7b717a1116db94120b8b516
Saitama32 <rostik743@gmail.com>
2026-04-10 00:28:04 +0300

    add success_frac to DQNAgent init

--- RL/rl_algorithms.py
+++ RL/rl_algorithms.py
@@ -31,7 +31,7 @@ TAU = 0.01
 class DQNAgent:
     def __init__(self, n_observation=None, n_action=None, optimizer_dict=None, lr=1e-3, gamma=0.95, epsilon=1.0,
                  epsilon_decay=0.995, epsilon_min=0.01, memory_size=50000, batch_size=128, n_transitions_reinit = 2000, per_alpha =  0.6, per_beta0 = 0.4, device='cpu', exp=None,
-                 warmup_updates: int = 50, recalc_batch_size: int = 32,):
+                 warmup_updates: int = 50, recalc_batch_size: int = 32, success_frac = 0.2):
         self.n_observation = n_observation
         self.n_action = n_action
         self.gamma = gamma
@@ -79,7 +79,7 @@ class DQNAgent:
 
         # --- Success replay (один буфер, логическая подселекция) ---
         # Доля последовательностей, которые берем из успешных эпизодов
-        self.success_frac = 0.3          # например, 30% батча
+        self.success_frac = success_frac         # например, 30% батча
         # Порог по model_reward, > которого done==1 считаем успехом
         self.success_reward_threshold = 0.0
         # прокидываем порог в буфер
