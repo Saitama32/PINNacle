11053aa66b4fd2867d798dcf9b67d998d71e100c
Saitama32 <rostik743@gmail.com>
2026-04-15 16:56:53 +0300

    Chnage agent exps keys for wave 1d

--- experiments/Wave/wave_1d_parallel_run.sh
+++ experiments/Wave/wave_1d_parallel_run.sh
@@ -4,8 +4,8 @@
 SCRIPT="experiments/Wave/wave_1d_chain.py"
 
 LOG_KEY="true"
-EXP_KEY_1="17c10318c0e14938b7cdd48c38c5ea99"
-EXP_KEY_2="8dd9b51dbc9c49d1ae639ebbd54fda51"
+EXP_KEY_1="0715d87731114f9abe013a4df4fb3b1b"
+EXP_KEY_2="a57b4324a5fc44cd89502c268cc24429"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
