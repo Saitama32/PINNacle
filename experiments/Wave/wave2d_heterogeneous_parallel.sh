a1f0768b4e4f3f0bf25b9ec743a3973dc2018f28
Saitama32 <rostik743@gmail.com>
2026-04-20 02:13:45 +0300

    change agent keys for wave2d cg

--- experiments/Wave/wave2d_heterogeneous_parallel.sh
+++ experiments/Wave/wave2d_heterogeneous_parallel.sh
@@ -4,8 +4,8 @@
 SCRIPT="experiments/Wave/wave2d_heterogeneous_chain.py"
 
 LOG_KEY="true"
-EXP_KEY_1="ea9574173098445aaac683c9dcd7dcda"
-EXP_KEY_2="49fe7c5104374bad88fb8decdf995a3c"
+EXP_KEY_1="b419965bf0f64a609f0a5e215360ae30"
+EXP_KEY_2="4a15f92e2fe2417a91e9253cf7d91d20"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
