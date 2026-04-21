c8480e17ca3c0ed55a832946c8fffabcff634ae3
Saitama32 <rostik743@gmail.com>
2026-04-22 01:25:43 +0300

    change seeds and agent keys for heat 2d vc

--- experiments/HeatConductivity_multi_agent_compare/heat2d_varyingcoef_parallel.sh
+++ experiments/HeatConductivity_multi_agent_compare/heat2d_varyingcoef_parallel.sh
@@ -4,8 +4,8 @@
 SCRIPT="experiments/HeatConductivity_multi_agent_compare/heat2d_varyingcoef_chain.py"
 
 LOG_KEY="true"
-EXP_KEY_1="17c10318c0e14938b7cdd48c38c5ea99"
-EXP_KEY_2="4ebd70b7eddd49c1a864a2ad7374d46c"
+EXP_KEY_1="fa84430ec078475b92cc4e0877112a27"
+EXP_KEY_2="94abad8c7f6642099af8a4ece6daa32f"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
