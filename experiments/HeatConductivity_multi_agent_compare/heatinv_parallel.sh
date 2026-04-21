b8e38422d9fb3871fb4ec790208d425543a6c63a
Saitama32 <rostik743@gmail.com>
2026-04-22 01:36:26 +0300

    change seeds and agent keys for heatinv

--- experiments/HeatConductivity_multi_agent_compare/heatinv_parallel.sh
+++ experiments/HeatConductivity_multi_agent_compare/heatinv_parallel.sh
@@ -4,8 +4,8 @@
 SCRIPT="experiments/HeatConductivity_multi_agent_compare/heatinv_chain.py"
 
 LOG_KEY="true"
-EXP_KEY_1="17c10318c0e14938b7cdd48c38c5ea99"
-EXP_KEY_2="a57b4324a5fc44cd89502c268cc24429"
+EXP_KEY_1="26b17a08e1e34dd78d4ec3f878251bc6"
+EXP_KEY_2="c7fccc5d9ee5411abe4916ae135c4f63"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
