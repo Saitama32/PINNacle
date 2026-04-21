8f1e31c6f0fb4daed0afc6697f1ff9eac45132bf
Saitama32 <rostik743@gmail.com>
2026-04-22 00:58:45 +0300

    change seeds for heat 2d long time

--- experiments/HeatConductivity_multi_agent_compare/heat2d_longtime_parallel.sh
+++ experiments/HeatConductivity_multi_agent_compare/heat2d_longtime_parallel.sh
@@ -4,8 +4,8 @@
 SCRIPT="experiments/HeatConductivity_multi_agent_compare/heat2d_longtime_chain.py"
 
 LOG_KEY="true"
-EXP_KEY_1="17c10318c0e14938b7cdd48c38c5ea99"
-EXP_KEY_2="a57b4324a5fc44cd89502c268cc24429"
+EXP_KEY_1="77456cdb097e4fb9b6a3d024d40b1566"
+EXP_KEY_2="1769a296dbf24188a000497243cf3af3"
 
 # Detect available GPUs
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
