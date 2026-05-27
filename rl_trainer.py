5eb9e45fc02ce2cbc48075650992d6438e59b8ae
Saitama32 <rostik743@gmail.com>
2026-05-27 17:33:12 +0300

    add optim after all steps

--- rl_trainer.py
+++ rl_trainer.py
@@ -319,6 +319,9 @@ def run_deepxde_rl_training(
                     f'Total reward after using {", ".join(optimizers_history)} '
                     f'{"optimizers" if len(optimizers_history) > 1 else "optimizer"}: {total_reward}.\n'
                     f'\ndone = {done}')
+            
+            if len(rl_agent.replay_buffer) >= rl_agent_params["agent_min_buffer"]:
+                rl_agent.optim_(iters=rl_agent_params["agent_update_iters"])
 
             # callbacks.callbacks[1].save_every = self.t
             # env.render()
