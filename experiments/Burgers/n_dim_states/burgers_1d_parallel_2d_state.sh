887d53ce4b1a762a5d70e6dcb9109b2a01210878
Saitama32 <rostik743@gmail.com>
2026-07-30 01:00:42 +0300

    change seeds for burgers 1d 2d state

--- experiments/Burgers/n_dim_states/burgers_1d_parallel_2d_state.sh
+++ experiments/Burgers/n_dim_states/burgers_1d_parallel_2d_state.sh
@@ -2,7 +2,7 @@
 # Auto-run RL chain and distribute jobs across available GPUs.
 
 SCRIPT="experiments/Burgers/n_dim_states/burgers_1d_chain_2d_state.py"
-SEEDS=(4321 54321)
+SEEDS=(432 5432)
 
 run_seed() {
     local gpu="$1"
