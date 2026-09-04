cbab35422a64915c0638023444a8414268d87d11
Saitama32 <rostik743@gmail.com>
2026-09-05 00:59:10 +0300

    add files to search tol for burgers 2d and ns2d backstep

--- experiments/NavierStokes/ns2d_backstep_chain.py
+++ experiments/NavierStokes/ns2d_backstep_chain.py
@@ -8,7 +8,7 @@ sys.path.append(project_root)
 from comet_config import start_comet_experiment
 
 experiment = start_comet_experiment(
-    project_name="rlpinn_ns2d_backstep_loss_ratio_reward_optimization",
+    project_name="rlpinn_ns2d_backstep_loss_chain_tolerance",
 )
 
 
