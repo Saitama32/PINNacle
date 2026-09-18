c73bedae3f8205c9c63baf64d9a20b8b0c9b2c17
Saitama32 <rostik743@gmail.com>
2026-09-18 23:38:38 +0300

    chnage proj and sh script for poisson inv

--- experiments/Poisson/poissoninv_chain.py
+++ experiments/Poisson/poissoninv_chain.py
@@ -5,7 +5,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..
 sys.path.append(project_root)
 from comet_config import start_comet_experiment
 experiment = start_comet_experiment(
-    project_name="rlpinn_poissoninv_loss_ratio_reward_optimization",
+    project_name="rlpinn_poissoninv_loss_chain_tolerance",
 )
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
